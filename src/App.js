70c524e6efffbc878a654ed71e634f390e6bb27d
Lisseth Martinez <lis.seth2k16@gmail.com>
2025-01-24 19:21:33 -0600

    Emtpy project working with App.js instead of App.tsx

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to React Native</Text>
+      <Text>Step one edit app.tsx to change this screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
